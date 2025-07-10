3841fed94349f6729aa29fa6c010450f8b2a2db7
SongDrop Development <dev.team@songdrop.band>
2025-07-10 14:24:13 +0100

    Remove prod as submodule and add as normal folder

---
+++ prod/delete_vm/__init__.py
@@ -0,0 +1,44 @@
+import asyncio
+import json
+import os
+import sys
+import time
+import logging
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+load_dotenv()  # This loads environment variables from a .env file in the current directory
+import random
+import string
+import subprocess
+import shutil
+import platform
+import webbrowser
+import dns.resolver
+from azure.core.exceptions import ClientAuthenticationError
+from azure.identity import ClientSecretCredential
+from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
+
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.network import NetworkManagementClient
+from azure.mgmt.network.models import NetworkSecurityGroup, SecurityRule, NetworkInterface
+from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.compute.models import (
+    VirtualMachine, HardwareProfile, StorageProfile,
+    OSProfile, NetworkProfile, NetworkInterfaceReference,
+    VirtualMachineExtension, WindowsConfiguration,SecurityProfile
+)
+from azure.mgmt.dns import DnsManagementClient
+from azure.mgmt.dns.models import RecordSet
+from azure.mgmt.storage import StorageManagementClient
+import azure.functions as func
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="delete_vm")
+def delete_vm(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    
+    return func.HttpResponse(
+        "Hello World DELETED",
+        status_code=200
+    )
