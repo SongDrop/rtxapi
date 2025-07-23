79b935cb295758bab7b02dce6a9b8459eeafcc0a
SongDrop Development <dev.team@songdrop.band>
2025-07-23 20:04:25 +0100

    api-gateway

--- api_gateway/__init__.py
+++ api_gateway/__init__.py
@@ -38,20 +38,17 @@ import azure.functions as func
 
 logger = logging.getLogger(__name__)
 
-def get_function_key(function_name, key_to_return: str = "default"):
+def get_function_keys(function_name):
     try:
-        # Authenticate using ClientSecretCredential
+        # Authenticate
         try:
             credentials = ClientSecretCredential(
-                client_id=os.environ['AZURE_APP_CLIENT_ID'],
-                client_secret=os.environ['AZURE_APP_CLIENT_SECRET'],
-                tenant_id=os.environ['AZURE_APP_TENANT_ID']
+                client_id=os.environ['AZURE_CLIENT_ID'],
+                client_secret=os.environ['AZURE_CLIENT_SECRET'],
+                tenant_id=os.environ['AZURE_TENANT_ID']
             )
         except KeyError as e:
-            err = f"Missing environment variable: {e}"
-            logger.error(err)
-            # Returning None instead of HttpResponse here; 
-            # HttpResponse should be returned from Azure Function main handler, not helper function
+            logger.error(f"Missing environment variable: {e}")
             return None
 
         subscription_id = os.environ.get('AZURE_SUBSCRIPTION_ID')
@@ -60,75 +57,60 @@ def get_function_key(function_name, key_to_return: str = "default"):
             return None
 
         client = WebSiteManagementClient(credentials, subscription_id)
-        keys = client.web_apps.list_function_keys(os.environ.get('API_RESOURCE_GROUP'), os.environ.get('API_NAME'), function_name)
-        
-        if keys and hasattr(keys, 'keys') and keys.keys:
-            default_key = keys.keys.get(key_to_return)
-            if default_key:
-                return default_key
+        keys = client.web_apps.list_function_keys(
+            os.environ.get('API_RESOURCE_GROUP'),
+            os.environ.get('API_NAME'),
+            function_name
+        )
+
+        if keys:
+            # keys may be dict or have attribute keys depending on SDK version
+            if hasattr(keys, 'keys') and keys.keys:
+                return keys.keys
+            elif isinstance(keys, dict):
+                return keys
             else:
-                logger.error(f"Key '{key_to_return}' not found for function '{function_name}'.")
+                logger.error("Unexpected keys structure")
                 return None
         else:
             logger.error(f"No keys found for function '{function_name}'.")
             return None
 
     except Exception as e:
-        logger.error(f"Exception fetching function key for '{function_name}': {e}", exc_info=True)
+        logger.error(f"Exception fetching keys for function '{function_name}': {e}", exc_info=True)
         return None
 
-#This function acts as a api-gateway
-#https://myapi-{azure_id}.uksouth-01.azurewebsites.net/api_gateway?code={code}&function_name={the_other_function}
-#you call this function with the other function name such as 'create_vm' in request and it will return back
-#https://myapi-{azure_id}.uksouth-01.azurewebsites.net/create_vm?code={default_sas_token}
-#here we are essentially giving back the correct api function link with the SAS token added
-
-#this approach also allow to do extra authentication for the proxy
-##https://myapi-{azure_id}.uksouth-01.azurewebsites.net/api_gateway?code={code}&function_name={the_other_function}&auth_token={your_auth_token}
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logger.info("Processing request to generate full function URL with key")
+    logger.info("Processing request to retrieve function keys")
 
     try:
         req_body = req.get_json()
     except ValueError:
         req_body = {}
 
-    # Get parameters from JSON body or query parameters
     function_name = req_body.get('function_name') or req.params.get('function_name')
     if not function_name:
         return func.HttpResponse(
-            "Please provide a function_name query parameter",
+            "Please provide a function_name parameter",
             status_code=400
         )
 
     if not all([SUBSCRIPTION_ID, API_RESOURCE_GROUP, API_NAME, API_DEFAULT_DOMAIN]):
-        logger.error("One or more required environment variables are missing.")
+        logger.error("Missing required environment variables")
         return func.HttpResponse(
-            "One or more required environment variables are missing.",
+            "Missing required environment variables",
             status_code=400
         )
-    #key_to_return = 'default' > you can create multiple keys as 
-    #well for each function app but we use default
-    #########
-    key_to_return = "default"
-    default_key = get_function_key(function_name, key_to_return)
-    if not default_key:
-        logger.error(f"No '{key_to_return}' key found for function '{function_name}'.")
+
+    keys = get_function_keys(function_name)
+    if not keys:
         return func.HttpResponse(
-            f"No'{key_to_return}' key found for function '{function_name}'.",
+            f"No keys found for function '{function_name}'",
             status_code=404
         )
 
-    base_url = f"https://{API_DEFAULT_DOMAIN}/{function_name}"
-    full_url = f"{base_url}?code={default_key}"
-    return func.HttpResponse(full_url, status_code=200)
-
-    method = req.method
-    if method == "GET":
-        return func.HttpResponse("create_vm: Received a GET request", status_code=200)
-    elif method == "POST":
-        return func.HttpResponse("create_vm: Received a POST request", status_code=200)
-    else:
-        return func.HttpResponse(f"create_vm: Received a {method} request", status_code=200)
-        return func.HttpResponse(f"Received a {method} request", status_code=200)
+    return func.HttpResponse(
+        json.dumps(keys),
+        mimetype='application/json',
+        status_code=200
+    )
