a317bf031808123ada8f023d3a4495af10b1374a
SongDrop Development <dev.team@songdrop.band>
2025-07-23 19:19:08 +0100

    api-gateway

--- api_gateway/__init__.py
+++ api_gateway/__init__.py
@@ -29,20 +29,20 @@ API_NAME = os.getenv("API_NAME") #your api name
 API_DEFAULT_DOMAIN = os.getenv("API_DEFAULT_DOMAIN") #your api name
 
 
-def get_function_key(function_name, key_to_return: str = "default"):
-    try:
-        credential = DefaultAzureCredential()
-        client = WebSiteManagementClient(credential, SUBSCRIPTION_ID)
-        keys = client.web_apps.list_function_keys(API_RESOURCE_GROUP, API_NAME, function_name)
-        if keys and hasattr(keys, 'keys') and keys.keys:
-            default_key = keys.keys.get(key_to_return)
-            return default_key
-        else:
-            logger.error(f"No keys found for function '{function_name}'.")
-            return None
-    except Exception as e:
-        logger.error(f"Exception fetching function key for '{function_name}': {e}")
-        return None
+# def get_function_key(function_name, key_to_return: str = "default"):
+#     try:
+#         credential = DefaultAzureCredential()
+#         client = WebSiteManagementClient(credential, SUBSCRIPTION_ID)
+#         keys = client.web_apps.list_function_keys(API_RESOURCE_GROUP, API_NAME, function_name)
+#         if keys and hasattr(keys, 'keys') and keys.keys:
+#             default_key = keys.keys.get(key_to_return)
+#             return default_key
+#         else:
+#             logger.error(f"No keys found for function '{function_name}'.")
+#             return None
+#     except Exception as e:
+#         logger.error(f"Exception fetching function key for '{function_name}': {e}")
+#         return None
 
 #This function acts as a api-gateway
 #https://myapi-{azure_id}.uksouth-01.azurewebsites.net/api_gateway?code={code}&function_name={the_other_function}
@@ -57,31 +57,40 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logger.info("Processing request to generate full function URL with key")
 
     # Get the function_name from query parameters, fallback to env var if not provided
-    function_name = req.params.get('function_name') or ''
-    if not function_name:
-        return func.HttpResponse(
-            "Please provide a function_name query parameter",
-            status_code=400
-        )
+    # function_name = req.params.get('function_name') or ''
+    # if not function_name:
+    #     return func.HttpResponse(
+    #         "Please provide a function_name query parameter",
+    #         status_code=400
+    #     )
 
-    if not all([SUBSCRIPTION_ID, API_RESOURCE_GROUP, API_NAME, API_DEFAULT_DOMAIN]):
-        logger.error("One or more required environment variables are missing.")
-        return func.HttpResponse(
-            "One or more required environment variables are missing.",
-            status_code=400
-        )
-    #key_to_return = 'default' > you can create multiple keys as 
-    #well for each function app but we use default
-    #########
-    key_to_return = "default"
-    default_key = get_function_key(function_name, key_to_return)
-    if not default_key:
-        logger.error(f"No '{key_to_return}' key found for function '{function_name}'.")
-        return func.HttpResponse(
-            f"No'{key_to_return}' key found for function '{function_name}'.",
-            status_code=404
-        )
+    # if not all([SUBSCRIPTION_ID, API_RESOURCE_GROUP, API_NAME, API_DEFAULT_DOMAIN]):
+    #     logger.error("One or more required environment variables are missing.")
+    #     return func.HttpResponse(
+    #         "One or more required environment variables are missing.",
+    #         status_code=400
+    #     )
+    # #key_to_return = 'default' > you can create multiple keys as 
+    # #well for each function app but we use default
+    # #########
+    # key_to_return = "default"
+    # default_key = get_function_key(function_name, key_to_return)
+    # if not default_key:
+    #     logger.error(f"No '{key_to_return}' key found for function '{function_name}'.")
+    #     return func.HttpResponse(
+    #         f"No'{key_to_return}' key found for function '{function_name}'.",
+    #         status_code=404
+    #     )
 
-    base_url = f"https://{API_DEFAULT_DOMAIN}/{function_name}"
-    full_url = f"{base_url}?code={default_key}"
-    return func.HttpResponse(full_url, status_code=200)
+    # base_url = f"https://{API_DEFAULT_DOMAIN}/{function_name}"
+    # full_url = f"{base_url}?code={default_key}"
+    # return func.HttpResponse(full_url, status_code=200)
+
+    method = req.method
+    if method == "GET":
+        return func.HttpResponse("create_vm: Received a GET request", status_code=200)
+    elif method == "POST":
+        return func.HttpResponse("create_vm: Received a POST request", status_code=200)
+    else:
+        return func.HttpResponse(f"create_vm: Received a {method} request", status_code=200)
+        return func.HttpResponse(f"Received a {method} request", status_code=200)
