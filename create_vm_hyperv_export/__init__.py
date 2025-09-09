97338d3bd3bbdcda95726e4515086e7215babe95
SongDrop Development <dev.team@songdrop.band>
2025-09-09 18:28:05 +0100

    create_vm_hyperv_snapshot

--- create_vm_hyperv_export/__init__.py
+++ create_vm_hyperv_export/__init__.py
@@ -13,6 +13,7 @@ import string
 import shutil
 import platform
 import dns.resolver
+from urllib.parse import quote_plus
 from azure.core.exceptions import ClientAuthenticationError
 from azure.identity import ClientSecretCredential
 from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions, generate_container_sas, ContainerSasPermissions
@@ -542,8 +543,9 @@ async def provision_vm_background(
         #SNAPSHOT_URL: Generated os-disk-snapshot download URL for Hyper-V
         #AZURE_SAS_TOKEN: Generated SAS Upload URL for az-copy uploading bootable-fixed-size.vhd
         #WEBHOOK_URL: Keep Front-End informed during auto-installation on Windows
+        encoded_sas = quote_plus(SNAPSHOT_URL)
         ps_script = generate_setup.generate_setup(
-            SNAPSHOT_URL=SNAPSHOT_URL,
+            SNAPSHOT_URL=encoded_sas,
             WEBHOOK_URL=hook_url,
             AZURE_SAS_TOKEN=VHD_EXPORT_SAS_URL
         )
