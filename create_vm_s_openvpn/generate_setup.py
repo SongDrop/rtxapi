0eea5c599e23c142afeb2c2fad542f2151987b4d
SongDrop Development <dev.team@songdrop.band>
2025-10-19 14:26:55 +0100

    create_vm_s_openvpn

--- create_vm_s_openvpn/generate_setup.py
+++ create_vm_s_openvpn/generate_setup.py
@@ -534,7 +534,7 @@ server {
     ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
 
     location / {
-        proxy_pass http://127.0.0.1:${PORT_UI};
+        proxy_pass http://127.0.0.1:__PORT_UI__;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
